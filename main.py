8785cb76df96648873548c0ebafad7eb3b17cd7f
Alejandro Socarras <apsocarras@gmail.com>
2022-11-04 11:49:47 -0400

    Complete list_sayer()

--- main.py
+++ main.py
@@ -1,23 +1,14 @@
-# Write the following in a file called main.py:
-
-#     A function called list_sayer() that takes a list as an argument
-#         If the list isn't empty (i.e. there's one or more items in it):
-#             print a string for each item saying the name of the item and its index
-#             return the Boolean True
-#         If the list is empty:
-#             print a string saying that the list is empty
-#             return the Boolean False
 
 def list_sayer(ls:list):
-  
+  if len(ls) > 0: 
+    for i in range(len(ls)):
+      print(f"Name: {ls[i]}, Index: {i}")
+    return True
+  else: 
+    print("The list is empty.")
+    return False
 
-#     A function called dict_sayer() that takes a dictionary as an argument
-#         If the dictionary isn't empty (i.e. there's one or more items in it):
-#             print a string for each item saying the name of each key and its value
-#             return the Boolean True
-#         If the dictionary is empty:
-#             print a string saying that the dictionary is empty
-#             return the Boolean False
+
 
 #     A function called greatest() that takes as an argument a dictionary that has strings as keys and integers as values
 #         find the greatest value
@@ -28,3 +19,4 @@ def list_sayer(ls:list):
 #             return a dictionary that has the items of the first list as keys, and each item in the same index in the second list as the value > For instance, the lists ["vanilla", "cherry"] and ["cake", "ice_cream"] would return {"vanilla": "cake", "cherry": "ice_cream"}
 #         If the lists are different lengths:
 #             return a tuple with each list and its length > For instance, the lists ["vanilla", "cherry", "pistachio"] and ["cake", "ice_cream"] would return (["vanilla", "cherry", "pistachio"], 3, ["cake", "ice_cream"], 4)
+
