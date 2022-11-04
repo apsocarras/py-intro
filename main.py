8a258ac873d5401ca8c15be0e4aa3aaec6e9f815
Alejandro Socarras <apsocarras@gmail.com>
2022-11-04 11:23:27 -0400

    Add main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+# Write the following in a file called main.py:
+
+#     A function called list_sayer() that takes a list as an argument
+#         If the list isn't empty (i.e. there's one or more items in it):
+#             print a string for each item saying the name of the item and its index
+#             return the Boolean True
+#         If the list is empty:
+#             print a string saying that the list is empty
+#             return the Boolean False
+
+def list_sayer(ls:list):
+  
+
+#     A function called dict_sayer() that takes a dictionary as an argument
+#         If the dictionary isn't empty (i.e. there's one or more items in it):
+#             print a string for each item saying the name of each key and its value
+#             return the Boolean True
+#         If the dictionary is empty:
+#             print a string saying that the dictionary is empty
+#             return the Boolean False
+
+#     A function called greatest() that takes as an argument a dictionary that has strings as keys and integers as values
+#         find the greatest value
+#         return a tuple with the greatest value and its key
+
+#     A function called zipper() that takes two lists as arguments
+#         If the lists are the same length:
+#             return a dictionary that has the items of the first list as keys, and each item in the same index in the second list as the value > For instance, the lists ["vanilla", "cherry"] and ["cake", "ice_cream"] would return {"vanilla": "cake", "cherry": "ice_cream"}
+#         If the lists are different lengths:
+#             return a tuple with each list and its length > For instance, the lists ["vanilla", "cherry", "pistachio"] and ["cake", "ice_cream"] would return (["vanilla", "cherry", "pistachio"], 3, ["cake", "ice_cream"], 4)
