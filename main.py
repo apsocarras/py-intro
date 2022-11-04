d63d77389b4814978131e810ef49c6bbfab5c7a1
Alejandro Socarras <apsocarras@gmail.com>
2022-11-04 12:44:55 -0400

    Add comments to functions

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 def list_sayer(ls:list):
-  if len(ls) > 0: 
+  if len(ls) > 0: # Since non-empty lists are truthy and empty lists are falsey, we could also write 'if ls:'
     for i in range(len(ls)):
       print(f"Name: {ls[i]}, Index: {i}")
     return True
@@ -9,7 +9,7 @@ def list_sayer(ls:list):
     return False
 
 def dict_sayer(d:dict): 
-  if len(d) > 0:
+  if len(d) > 0: # Empty dictionaries are falsy and non-empty dictionaries falsey, so we could also write 'if d:' 
     for key, value in d.items():
       print(f"Key name: {key}, Value: {value}")
     return True
@@ -18,8 +18,11 @@ def dict_sayer(d:dict):
     return False
 
 def greatest(d:dict):
+   # Note that there could be multiple entries in the dict sharing the "greatest value"
   max_val = max(d.values())
+  # Because this list comprehension produces a set, which is unordered, the order of the keys might be changed: 
   matching_keys = list({key for key in d if d[key] == max_val}) 
+  # Hence, matching_keys[0] might not be the first key listed in the dictionary that has the greatest value 
   return (max_val, matching_keys[0])
 
 def zipper(l1:list, l2:list):
